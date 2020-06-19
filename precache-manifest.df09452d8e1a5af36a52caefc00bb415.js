e7bc150c79409fe0ae513618d628800be046e514
GorAraqelyan <50023373+GorAraqelyan@users.noreply.github.com>
2020-06-19 15:37:59 +0400

    Updates

---
+++ precache-manifest.df09452d8e1a5af36a52caefc00bb415.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb1ec2ad961267cb6ce81d9603ba6f32",
+    "url": "/type-racer/index.html"
+  },
+  {
+    "revision": "8c2a76d94a096074a28a",
+    "url": "/type-racer/static/css/main.bdd615aa.chunk.css"
+  },
+  {
+    "revision": "83e86659b7e32f86e095",
+    "url": "/type-racer/static/js/2.73bfe6f9.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/type-racer/static/js/2.73bfe6f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c2a76d94a096074a28a",
+    "url": "/type-racer/static/js/main.fb79d26b.chunk.js"
+  },
+  {
+    "revision": "d0541809e78d8c7168b3",
+    "url": "/type-racer/static/js/runtime-main.4e37bc27.js"
+  }
+]);
